e9413ca2a81e0480ac32e0497954339bc1df0e2a
mmmirellla <mpsirella@gmail.com>
2024-03-12 20:52:59 -0300

    Adicionar arquivos de projeto.

---
+++ CalculoFatorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// método com retorno; método estático
+namespace Fatoriais
+{
+    public class CalculoFatorial
+    {
+        public static int Fatorial(int n)
+        {
+            int resultado = 1;
+            while (n != 1)
+            {
+                resultado = resultado * n;
+                n = n - 1;
+            }
+            return resultado;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+using Fatoriais;
+
+Console.WriteLine("Bem vindo ao programa de Fatoração de 1 a 10!");
+Console.WriteLine("");
+Console.WriteLine("Qual número (de 1 a 10) você quer fatorar?");
+int n = Convert.ToInt32(Console.ReadLine());
+   
+Console.WriteLine($"{n}! = " + CalculoFatorial.Fatorial(n));
